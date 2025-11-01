da3c70bf385a7ff9f9338330cab46ae62189f3e2
PAsupathi <pasupathifeathers@gmail.com>
2025-11-01 23:38:36 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,6 +21,8 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
